09b1518e13e6409f79b143aa30f2fc754b61483c
yuanqi <mutouyuguo@gmail.com>
2019-04-20 23:50:42 +0800

    use prettier as coding standard

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,27 @@
 module.exports = {
-    "env": {
-        "browser": true,
-        "es6": true,
-        "amd": true,
-        "node": true
-    },
-    "extends": "eslint:recommended",
-    "parserOptions": {
-        "ecmaVersion": 2018,
-        "sourceType": "module"
-    },
-    "rules": {
-        "indent": [
-            "error",
-            "tab"
-        ],
-        "linebreak-style": [
-            "error",
-            "unix"
-        ],
-        "quotes": [
-            "error",
-            "single"
-        ],
-        "semi": [
-            "error",
-            "always"
-        ]
-    }
+  env: {
+    browser: true,
+    es6: true,
+    amd: true,
+    node: true
+  },
+  extends: [
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    "standard",
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    "plugin:prettier/recommended"
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: "module"
+  },
+  plugins: ["prettier"],
+  rules: {
+    "prettier/prettier": "error",
+    indent: ["error", "tab"],
+    "linebreak-style": ["error", "unix"],
+    quotes: ["error", "single"],
+    semi: ["error", "always"]
+  }
 };
--- webpack.config.js
+++ webpack.config.js
@@ -1,88 +1,101 @@
-const HtmlWebPackPlugin = require('html-webpack-plugin');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 module.exports = {
-	devServer: {
-		contentBase: './dist'
-	},
-	module: {
-		rules: [
-			{
-				test: /\.js$/,
-				exclude: /node_modules/,
-				use: {
-					loader: 'babel-loader'
-				}
-			},
-			{
-				test: /\.html$/,
-				use: [
-					{
-						loader: 'html-loader',
-						options: { minimize: true }
-					}
-				]
-			},
-			{
-				test: /\.scss$/,
-				use: [{
-					loader: MiniCssExtractPlugin.loader
-				}, {
-					loader: 'css-loader',
-					options: {
-						sourceMap: true
-					}
-				}, {
-					loader: 'postcss-loader',
-					options: {
-						sourceMap: true,
-						plugins: loader => [
-						  require('autoprefixer')({
-                            browsers: [
-                                'last 10 Chrome versions',
-                                'last 5 Firefox versions',
-                                'Safari >= 6',
-                                'ie > 8'
-                            ]
-                          })
-						]
-					}
-				}, {
-					loader: 'sass-loader',
-					options: {
-						sourceMap: true
-					}
-				}]
-			},
-			{
-				test: /\.(png|svg|jpg|gif)$/,
-				use: [
-				'file-loader'
-				]
-			}
-		]
-	},
-	plugins: [
-		new HtmlWebPackPlugin({
-			template: './src/index.html',
-			filename: './index.html'
-		}),
-		new MiniCssExtractPlugin({
-			filename: 'styles.css',
-			chunkFilename: '[id].css'
-		})
-	],
-	optimization: {
-		minimizer: [
-		 new OptimizeCSSAssetsPlugin({
-		 	cssProcessor: require('cssnano'),
-			    cssProcessorOptions: {
-			        discardComments: { removeAll: true },
-			        // 避免 cssnano 重新计算 z-index
-			        safe: true,
-			        autoprefixer: false
-			    },
-		    }),
-		]
-	}
+  devServer: {
+    contentBase: "./dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "eslint-loader",
+            enforce: "pre",
+            include: [path.join(__dirname, "src")],
+            options: {
+              fix: true
+            }
+          },
+          {
+            loader: "babel-loader"
+          }
+        ]
+      },
+      {
+        test: /\.html$/,
+        use: [
+          {
+            loader: "html-loader",
+            options: { minimize: true }
+          }
+        ]
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          {
+            loader: "css-loader",
+            options: {
+              sourceMap: true
+            }
+          },
+          {
+            loader: "postcss-loader",
+            options: {
+              sourceMap: true,
+              plugins: loader => [
+                require("autoprefixer")({
+                  browsers: [
+                    "last 10 Chrome versions",
+                    "last 5 Firefox versions",
+                    "Safari >= 6",
+                    "ie > 8"
+                  ]
+                })
+              ]
+            }
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: ["file-loader"]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebPackPlugin({
+      template: "./src/index.html",
+      filename: "./index.html"
+    }),
+    new MiniCssExtractPlugin({
+      filename: "styles.css",
+      chunkFilename: "[id].css"
+    })
+  ],
+  optimization: {
+    minimizer: [
+      new OptimizeCSSAssetsPlugin({
+        cssProcessor: require("cssnano"),
+        cssProcessorOptions: {
+          discardComments: { removeAll: true },
+          // 避免 cssnano 重新计算 z-index
+          safe: true,
+          autoprefixer: false
+        }
+      })
+    ]
+  }
 };
